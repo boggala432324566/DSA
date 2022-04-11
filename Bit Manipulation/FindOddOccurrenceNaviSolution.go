35babeab378c2fd7ddb1731a07645229dc77907d
BoggalaPrabhakar007 <40452084+BoggalaPrabhakar007@users.noreply.github.com>
2022-04-11 16:02:53 +0530

    Create FindOddOccurrenceNaviSolution.go

---
+++ Bit Manipulation/FindOddOccurrenceNaviSolution.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"	
+)
+
+func main() {
+	FindOddOccurrence()
+}
+
+func FindOddOccurrence() {
+	var arr = []int32{2, 2, 3, 5, 5, 6, 6, 7, 7, 7, 7}
+	for i := 0; i < len(arr); i++ {
+		count := 0
+		for j := 0; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				count++
+			}
+		}
+		if count%2 != 0 {
+			fmt.Println(arr[i])
+		}
+	}
+}
